02d85459884fb429065380dcb9bb9701485aef4f
devRezaulKarim <dev.rezaulkarim@gmail.com>
2025-10-15 06:55:31 +0600

    Feat(auth): enhance auth layout with background image and logo
    - Added ImageBackground and logo to the auth layout for improved UI.
    - Wrapped layout in KeyboardAvoidingView and ScrollView for better UX.
    - Updated Appwrite config with database and user collection IDs.

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
   platform: "com.rkz.fastfood",
+  database: process.env.EXPO_PUBLIC_APPWRITE_DB_ID,
+  userCollectionId: process.env.EXPO_PUBLIC_APPWRITE_DB_USER_TBL_ID,
 };
