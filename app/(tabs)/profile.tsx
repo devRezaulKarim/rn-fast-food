3c5da8ec641b78d2af7cb881df8b3e93fea1c0e1
devRezaulKarim <dev.rezaulkarim@gmail.com>
2025-10-20 19:42:55 +0600

    feat(profile): add profile screen UI and ProfileField component
    - Added `ProfileField` component to display labeled user info fields.
    - Updated `Profile` screen with user avatar and styled info section.
    - Integrated `CustomHeader` for consistent header design.
    - Improved profile layout with scroll view and shadow effects.

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,11 +1,38 @@
+import CustomHeader from "@/components/CustomHeader";
+import ProfileField from "@/components/ProfileField";
+import useAuthStore from "@/store/auth.store";
 import React from "react";
-import { Text } from "react-native";
+import { Image, Platform, ScrollView, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Profile() {
+  const { user } = useAuthStore();
+
   return (
     <SafeAreaView className="bg-white h-full">
-      <Text>Profile</Text>
+      <ScrollView className="px-4">
+        <CustomHeader title="Profile" />
+        <View className="flex-center">
+          <Image source={{ uri: user?.avatar }} className="profile-avatar" />
+        </View>
+        <View
+          className="bg-white shadow-md shadow-dark-100/10 mt-8 mx-4 px-4 py-6 rounded-2xl gap-8"
+          style={
+            Platform.OS === "android"
+              ? { elevation: 5, shadowColor: "#878787" }
+              : {}
+          }
+        >
+          <ProfileField icon="user" label="Full Name" value={user?.name!} />
+          <ProfileField icon="envelope" label="Email" value={user?.email!} />
+          <ProfileField icon="phone" label="Phone Number" value="+1234567890" />
+          <ProfileField
+            icon="location"
+            label="Address 1 "
+            value="123 Main Street, Uttara, Dhaka 1230"
+          />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
---
+++ components/ProfileField.tsx
@@ -0,0 +1,27 @@
+import { images } from "@/constants";
+import React from "react";
+import { Image, Text, View } from "react-native";
+
+interface ProfileFieldProps {
+  icon: keyof typeof images;
+  label: string;
+  value: string;
+}
+
+const ProfileField = ({ icon, label, value }: ProfileFieldProps) => {
+  return (
+    <View className="flex-row items-center gap-x-4 w-full ">
+      <View className="bg-primary/10 p-4 rounded-full shrink-0">
+        <Image source={images[icon]} className="size-10" resizeMode="contain" />
+      </View>
+      <View>
+        <Text className="text-base text-start font-quicksand-medium text-gray-100">
+          {label}
+        </Text>
+        <Text className="font-quicksand-bold text-dark-100">{value}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default ProfileField;
